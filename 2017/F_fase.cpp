407d2639203448ea4108612c18900ce9e4f8f1b9
breno <joao.breno.ro17@gmail.com>
2020-09-28 15:53:07 -0300

    add problem F

---
+++ 2017/F_fase.cpp
@@ -0,0 +1,31 @@
+/*
+    Em uma competição, de N competidores, no mínimo K competidores de classificam.
+    Em caso de empate na última colocação, todos passam..
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+int main() {
+ 
+    int n,k,x,i;
+    vector<int> pts(1000, 0); // vetor de 1000 posições com valor 0 em cada uma
+    
+    cin >> n >> k;
+
+    for(i = 0; i < n; i++){
+        cin >> x;
+        pts[-x +1000] += 1; // mais um competidor com pontuação igual a -x +1000
+    }
+
+    x = i = 0;
+    while (x < k) {
+        x += pts[i++]; // conta quantos competidores se classificarão
+    }
+    
+    cout << x << endl;
+
+    return 0;
+}
